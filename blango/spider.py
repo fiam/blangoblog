c24240c689ad825ede433c54de740ec918ed557b
Alberto García Hierro <fiam@rm-fr.net>
2008-03-30 23:38:13 +0200

    Fix spider for links referencing a HTML id.

--- blango/spider.py
+++ blango/spider.py
@@ -52,6 +52,8 @@ class BaseSpider(object):
             self.uri = uri
         else:
             self.uri = 'http://%s' % uri
+        if self.uri.find('#'):
+            self.uri = self.uri[:self.uri.find('#')]
         self.request = urllib2.Request(self.uri)
         self.request.add_header('User-Agent', 'Blango/0.1 +http://blango.net')
         self.request.add_header('Accept-encoding', 'gzip')
